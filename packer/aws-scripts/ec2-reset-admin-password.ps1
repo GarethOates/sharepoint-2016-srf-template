cb8172d18df2d67d96495333c867fb23ed77b3e8
Gareth Oates <gaoa@sivilrett.no>
2020-11-19 00:53:25 +0100

    Remove AWS related files

--- packer/aws-scripts/ec2-reset-admin-password.ps1
+++
@@ -1,6 +0,0 @@
-$launchConfig = Get-Content -Path C:\ProgramData\Amazon\EC2-Windows\Launch\Config\LaunchConfig.json | ConvertFrom-Json
-$launchConfig.adminPasswordType = 'Specify'
-$launchConfig.adminPassword = 'Pass@word1!'
-$launchConfig
-
-Set-Content -Value ($launchConfig | ConvertTo-Json) -Path C:\ProgramData\Amazon\EC2-Windows\Launch\Config\LaunchConfig.json
